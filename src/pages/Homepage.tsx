87e96384c28e35b438b1406dc6e4cfe2c8ef3d5a
devJennyy <devjenny.official@gmail.com>
2024-06-23 12:25:54 +0800

    Updated UI design of the Homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,35 +1,84 @@
+import { LuArrowUpRight } from "react-icons/lu";
+
 const Homepage = () => {
   return (
-    <div className="max-w-[1536px] mx-auto h-full text-[#E2E4E9] relative">
-      <div className="flex flex-col justify-center text-left w-2/3 h-full pl-[10rem] gap-[4rem]">
-        <div className="flex flex-col gap-3">
-          <p className="text-[18px] font-medium">Hi, my name is</p>
-          <p className="text-[70px] font-bold">Jenny Pieloor</p>
-          <p className="text-[30px] font-regular text-[#B1BAED]">
-            Turning concepts into captivating web realities.
-          </p>
+    <div className="w-full h-full">
+      {/* Navbar */}
+      <div className="w-full h-[100px]"></div>
+
+      <div className="flex justify-between w-full h-full">
+        {/* Left Ornaments */}
+        <div className="w-[24rem] h-full relative">
+          <img
+            src="/vectors/ornament-medium-one.svg"
+            className="bg-cover absolute bottom-[36.8rem] left-[3.3rem]"
+          />
+          <img
+            src="/vectors/ornament-small.svg"
+            className="bg-cover absolute top-[5.8rem] right-[10px]"
+          />
+          <img
+            src="/vectors/ornament.svg"
+            className="bg-cover absolute top-[14.7rem] right-[2.7rem]"
+          />
+          <img
+            src="/vectors/ornament-big.svg"
+            className="bg-cover absolute bottom-[25.7rem] left-[7.4rem]"
+          />
         </div>
 
-        <div className="gradient-border-bottom"></div>
+        {/* Content */}
+        <div className="min-w-[920px] pl-[100px] h-full">
+          <div className="flex flex-col w-full h-full text-left z-50">
+            <div className="flex flex-col mt-[10.3rem] gap-3">
+              <p className="text-[1rem] font-medium gradient-text">
+                Hi, my name is
+              </p>
+              <p className="text-[4.1rem] font-bold text-[#B1BAED]">
+                Jenny Pieloor
+              </p>
+              <p className="text-[4.1rem] font-bold mt-[-24px] text-[#B1BAED]/60">
+                I am a Frontend Dev.
+              </p>
+              <p className="text-[1rem] font-light w-[37.4rem] mt-5 text-[#B1BAED]/60">
+                I specialize in turning concepts into engaging web realities.
+                With expertise in modern web technologies and responsive design,
+                I strive to create user-friendly interfaces that deliver
+                exceptional digital experiences.
+              </p>
+            </div>
 
-        <div className="flex flex-col gap-10">
-          <p className="text-[20px] font-light">
-            A passionate Front-end Developer and UI/UX Designer.
-          </p>
-          <div className="flex gap-3 font-regular w-full h-[3.2rem]">
-            <button className="flex justify-center items-center w-[16.5rem] h-full gradient-border">
-              <div className="average-content gap-3">
-                <p>View all works</p>
-                <img src="/icons/view.svg" className="w-[1.3rem]" />
-              </div>
-            </button>
-            <button className="flex justify-center items-center w-[16.5rem] h-full gradient-all-borders">
-              <div className="content gap-3">
-                <p>Hire Me</p>
-                <img src="/icons/contact.svg" className="w-[1rem]" />
+            <button className="w-[14.5rem] h-[3.3rem] mt-[3rem] gradient-all-borders text-[#B1BAED] ">
+              <div className="flex-row-reverse justify-center items-center w-full h-full content gap-2">
+                <LuArrowUpRight size={18} className="ml-[-5px]" />
+                <p>Hire me</p>
               </div>
             </button>
           </div>
+        </div>
+
+        {/* Right Ornaments */}
+        <div className="w-[35rem] h-full">
+          <img
+            src="/vectors/ornament-medium-two.svg"
+            className="bg-cover absolute bottom-[29.6rem] right-[21.4rem]"
+          />
+          <img
+            src="/vectors/ornament-extrasmall.svg"
+            className="bg-cover absolute bottom-[30rem] right-[19.5rem]"
+          />
+          <img
+            src="/vectors/ornament.svg"
+            className="bg-cover absolute bottom-[25.7rem] right-[33.5rem]"
+          />
+          <img
+            src="/vectors/ornament-medium-three.svg"
+            className="bg-cover absolute bottom-[10.2rem] right-[17.8rem]"
+          />
+          <img
+            src="/vectors/ornament-regularsmall.svg"
+            className="bg-cover absolute bottom-[14.4rem] right-[30.3rem]"
+          />
         </div>
       </div>
     </div>
